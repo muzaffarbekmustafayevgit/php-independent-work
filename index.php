5854bbf91139b6701c136d17313d0fb7c19f7ac4
Muzaffar <https://github.com/muzaffarbekmustafayevgit>
2024-03-23 10:41:50 +0500

    my first commit

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+/*
+//begin 1
+// Kvadratning tomoni
+$a = 5; 
+// Kvadratning perimetri topish formulasi
+$P = 4 * $a;
+echo "Kvadratning perimetri: " . $P;
+*/
+
+/*
+//begin 2
+// Kvadratning tomoni
+$a = 5; 
+// Kvadratning yuzasini topish formulasi
+$S = $a * $a;
+echo "Kvadratning yuzasi: " . $S;
+*/
+
+/*
+//begin 3
+$a = 5; 
+$b = 3; 
+// To'g'ri to'rtburchakning yuzasi va perimetrini topish formulasi
+$S = $a * $b;
+$P = 2 * ($a + $b);
+echo "To'g'ri to'rtburchakning yuzasi: " . $S . "\n";
+echo "To'g'ri to'rtburchakning perimetri: " . $P . "\n";
+*/
+
+/*
+//begin 4
+$d = 10;
+// Pi (π) ning qiymati 
+$pi = 3.14;
+// Aylananing uzunligi topish formulasi
+$L = $pi * $d;
+echo "Aylananing uzunligi: " . $L;
+*/
+
+/*
+//begin 5
+
+
+*/
+?>
+</body>
+</html>
